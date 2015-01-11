375272e127e985ff9e59b4b0c27161dd9d526d97
Huggeh <fredrik.sa.rydh@gmail.com>
2015-01-11 17:47:10 +0100

    update

--- src/main/java/HelloSpark.java
+++ src/main/java/HelloSpark.java
@@ -1,5 +1,6 @@
+
 public class HelloSpark {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
 }
