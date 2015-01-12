d8f2a16fadb7e7fd3dfc3ce9992ee7f5e2bc429a
Huggeh <fredrik.sa.rydh@gmail.com>
2015-01-12 03:12:35 +0100

    added som route mappings, works with put from curl

--- src/main/java/HelloSpark.java
+++ src/main/java/HelloSpark.java
@@ -2,12 +2,30 @@ import static spark.Spark.*;
 import spark.*;
 
 public class HelloSpark {
+	public static String settedvalue = "Hello Spark MVC Framework!";
     public static void main(String[] args) {
-        get(new Route("/hello") {
+    	get(new Route("/hello") {
             @Override
             public Object handle(Request request, Response response) {
-                return "Hello Spark MVC Framework!";
+            	response.type("text");
+                return settedvalue;
             }
         });
+    	put(new Route("/change") {
+    		@Override
+    		public Object handle(Request request, Response response) {
+    			settedvalue = request.queryParams("val");
+    			response.status(201);
+    			return "Settedvalue set to '" + settedvalue + "'";
+    		}
+    	});
+    	get(new Route("/kitty") {
+            @Override
+            public Object handle(Request request, Response response) {
+            	response.type("text");
+                return settedvalue;
+            }
+        });
+        
     }
 }
